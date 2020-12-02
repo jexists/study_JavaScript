ea549c7f0ad049bedc686d5260894673bfdaf0eb
jexists <jexists486@gmail.com>
2020-12-02 23:14:45 +0900

    word chain  done

--- word_chain/index.js
+++ word_chain/index.js
@@ -3,14 +3,16 @@
 let first_word = "";
 let next_word = "";
 let wordChninGo = true;
-const textInput = document.getElementById("next_word");
+const textInput = document.getElementById("word_type");
 
 textInput.addEventListener('keypress', (e) => {
+  // keypress: 영어로 쓸경우 안되고 한국어만 가능
+  // keyup: 영어 끝말잇기 할경우 사용
+
   e.preventDefault();
   e.stopPropagation();
 
   if (e.key === 13 || e.keyCode === 13) {
-    console.log('첫글자', !first_word);
     if (!first_word) {
       first_word = textInput.value;
       createElement(first_word);
@@ -23,13 +25,19 @@ textInput.addEventListener('keypress', (e) => {
 
 function onWordChain(first, next_word) {
   console.log('첫', first, '다음', next_word);
-    if (first[first.length - 1] == next_word[0]){
-      createElement(next_word);
-      first_word = next_word;
-    } else {
-      textInput.value = null;
-      console.log('wrong');
+  if (first[first.length - 1] == next_word[0]) {
+    createElement(next_word);
+    first_word = next_word;
+  } else {
+    textInput.value = null;
+    if (wordChninGo) {
+      let li = document.createElement('li');
+      li.classList.add('red');
+      li.append('실패');
+      document.querySelector('.word_list').append(li);
     }
+    wordChninGo = false;
+  }
 };
 
 function createElement(data) {
