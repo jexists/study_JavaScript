1054dedfd0bf456460925c8429762fb124fe9d99
jexists <jexists486@gmail.com>
2020-12-28 21:20:28 +0900

    changed folder name, polygon

---
+++ polygon_rotate/index.js
@@ -0,0 +1,37 @@
+console.log('스크립트 연결 완료!')
+
+import { Polygon } from './polygon';
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+    
+    window.addEventListener('resize', this.resize.bind(this), false);
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+  }
+}
+
+window.onload = () => {
+  new App();
+}
---
+++ polygon_rotate/polygon.js
@@ -0,0 +1,32 @@
+const PI2 = Math.PI * 2;
+
+export class Polygon {
+  constructor(x, y, radius, sides) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.sides = sides;
+    this.rotate = 0;
+  }
+
+  animate(ctx) {
+    ctx.save();
+    ctx.fillStyle = '#333';
+    ctx.beginPath();
+
+    const angle = PI2 / this.sides;
+
+    ctx.translate(this.x, this.y);
+
+    for (let i = 0; i < this.sides; i++) {
+      const x = this.radius * Math.cos(angle * i);
+      const y = this.radius * Math.sin(angle * i);
+
+      (i == 0) ? ctx.moveTo(x, y) : ctx.lineTo(x,y);
+    }
+
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore();
+  }
+}
