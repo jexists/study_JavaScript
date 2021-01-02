924ffe9cec68e16462b58cebb543ba4b344470e1
jexists <jexists486@gmail.com>
2021-01-02 19:52:06 +0900

    clock js

--- analog_clock/index.js
+++ analog_clock/index.js
@@ -16,4 +16,33 @@ setInterval(() => {
   sc.style.transform = `rotateZ(${ss}deg)`;
 
   // console.log(`${hh+(mm/12)}시 ${mm}분 ${ss}초`);
-},1000)
+}, 1000);
+
+
+// //////////////////////////////////
+
+const secondHand = document.querySelector('.sec-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+
+}
+
+setInterval(setDate, 1000);
