e4aadd1ec2ed4d4700fb75f1149031d80688bf48
jexists <jexists486@gmail.com>
2021-01-04 23:08:06 +0900

    create error

--- todolist/script/index.js
+++ todolist/script/index.js
@@ -1,105 +1,115 @@
-const storageName = 'ToDoList';
 
-// elelment 
-const viewWrap = document.querySelector('.view_wrap');
-const createWrap = document.querySelector('.create_wrap');
-const dropdown = document.querySelector('.dropdown');
+(() => {
 
-const toDoform = document.querySelector("#createForm");
-const toDoTitle = toDoform.querySelector(".title");
-const toDoContents = toDoform.querySelector("#contents");
-const toDoListArea = document.querySelector(".list_area");
-    
-let toDoLists = []; 
+	const storageName = 'ToDoList';
+
+	// elelment 
+	const viewWrap = document.querySelector('.view_wrap');
+	const createWrap = document.querySelector('.create_wrap');
+	const dropdown = document.querySelector('.dropdown');
+
+	const toDoform = document.querySelector("#createForm");
+	const toDoTitle = toDoform.querySelector(".title");
+	const toDoContents = toDoform.querySelector("#contents");
+	const toDoListArea = document.querySelector(".list_area");
+	const addBtn = document.querySelector(".add_btn");
+	const cancleBtn = document.querySelector(".cancle_btn");
+
+	let toDoLists = [];
 
 
-const addTodoList = () => {
-  viewWrap.classList.add('display_none');
-  createWrap.classList.remove('display_none');
-}
-
-const cancleCreate = () => {
-  viewWrap.classList.remove('display_none');
-  createWrap.classList.add('display_none');
-
-	toDoTitle.value = "";
-	toDoContents.value = "";
-}
-
-const createTodo = (title, contents) => {
-  const li = document.createElement("li");
-	const delBtn = document.createElement("button");
-	const span = document.createElement("span");
-	const newId = toDoLists.length + 1;
-	delBtn.innerText = "완료";
-	delBtn.addEventListener("click", deleteTodo);
-
-	span.innerText = title;
-	li.appendChild(delBtn);
-	li.appendChild(span);
-
-	li.id = newId;
-  toDoListArea.appendChild(li);
-  
-  const toDoItem = { 
-    id: newId,
-    title: title,
-    createDate: Date.now(),
-    deadlineDate: Date.now(),
-    contents: contents,
-    alramYN: false,
-    completeYN: false,
-    starYN: false
-  };
-
-	toDoLists.push(toDoItem);
-	submitTodo();
-}
-
-const deleteTodo = (event) => {
-	const btn = event.target;
-	const li = btn.parentNode;
-	toDoListArea.removeChild(li);
-
-	const cleanToDos = toDoLists.filter(function(toDo){
-		return toDo.id !== parseInt(li.id); 
-	});
-	toDoLists = cleanToDos;
-	submitTodo()
-}
-
-const submitTodo = () => {
-	localStorage.setItem(storageName, JSON.stringify(toDoLists));
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if (!toDoTitle.value) {
-    console.log('no value');
-    return;
-  }
-	const title = toDoTitle.value;
-	const contents = toDoContents.value;
-	createTodo(title, contents);
-	toDoTitle.value = "";
-	toDoContents.value = "";
-}
-
-const loadToDoLists = () => {
-	const loadToDos = localStorage.getItem(storageName);
-	if (loadToDos !== null){
-		const parsedToDos = JSON.parse(loadToDos);
-		parsedToDos.forEach(function(toDo){
-			createTodo(toDo.title, toDo.contents);
-		});
+	const openModal = () => {
+		viewWrap.classList.add('display_none');
+		createWrap.classList.remove('display_none');
 	}
-}
 
-const init = () => {
+	const closeModal = () => {
+		viewWrap.classList.remove('display_none');
+		createWrap.classList.add('display_none');
+
+		toDoTitle.value = "";
+		toDoContents.value = "";
+	}
+
+	const createTodoLists = (title) => {
+		const newId = toDoLists.length + 1;
+		const html = `
+			<li id="todo_${newId}">
+				<span>${title}</span>
+				<button type="button">수정</button>
+				<button type="button">삭제</button>
+			</li>
+		`
+		//돌려야하나..? 고민....해봐야겟네...
+		toDoListArea.innerHTML = html;
+
+		const toDoItem = {
+			id: newId,
+			title: title,
+			createDate: Date.now(),
+			deadlineDate: Date.now(),
+			contents: contents,
+			alramYN: false,
+			completeYN: false,
+			starYN: false
+		};
+
+		toDoLists.push(toDoItem);
+		saveTodoLists();
+	}
+
+	const deleteTodo = (event) => {
+		const btn = event.target;
+		const li = btn.parentNode;
+		toDoListArea.removeChild(li);
+
+		const cleanToDos = toDoLists.filter(function (toDo) {
+			return toDo.id !== parseInt(li.id);
+		});
+		toDoLists = cleanToDos;
+		createTodoLists()
+	}
+
+	const saveTodoLists = () => {
+
+		localStorage.setItem(storageName, JSON.stringify(toDoLists));
+	}
+
+	const handleSubmit = (e) => {
+		console.log('??');
+		e.preventDefault();
+		if (!toDoTitle.value) {
+			console.log('no value');
+			return;
+		}
+		const title = toDoTitle.value;
+		const contents = toDoContents.value;
+		createTodoLists(title);
+		toDoTitle.value = "";
+		toDoContents.value = "";
+	}
+
+	const loadToDoLists = () => {
+		const storage = localStorage.getItem(storageName);
+
+		if (!!storage) {
+			const parsedToDos = JSON.parse(storage);
+			parsedToDos.forEach((toDo) => {
+				createTodoLists(toDo.title);
+			});
+		}
+	}
+
+
+	// init ========
 	loadToDoLists();
-}
 
-init();
+	toDoform.addEventListener("submit", handleSubmit);
+	toDoform.addEventListener("click", openModal);
+	addBtn.addEventListener("click", createTodoLists);
+	cancleBtn.addEventListener("click", closeModal);
 
 
+
+})();
 
