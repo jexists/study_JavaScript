c672571818e022fd5a3964beba862c29a62325ac
jexists <jexists486@gmail.com>
2021-01-07 22:50:09 +0900

    fixed name and add star fn

--- todolist/script/index.js
+++ todolist/script/index.js
@@ -1,43 +1,40 @@
+const storageName = 'ToDoList';
 
-// (() => {
+// elelment 
+const viewWrap = document.querySelector('.view_wrap');
+const createWrap = document.querySelector('.create_wrap');
+const dropdown = document.querySelector('.dropdown');
+const starYN = document.querySelector('.star');
 
-	const storageName = 'ToDoList';
+const toDoform = document.querySelector("#createForm");
+const toDoTitle = toDoform.querySelector(".title");
+const toDoContents = toDoform.querySelector("#contents");
+const toDoListArea = document.querySelector(".list_area");
+const addBtn = document.querySelector(".add_btn");
+const cancleBtn = document.querySelector(".cancle_btn");
 
-	// elelment 
-	const viewWrap = document.querySelector('.view_wrap');
-	const createWrap = document.querySelector('.create_wrap');
-	const dropdown = document.querySelector('.dropdown');
-
-	const toDoform = document.querySelector("#createForm");
-	const toDoTitle = toDoform.querySelector(".title");
-	const toDoContents = toDoform.querySelector("#contents");
-	const toDoListArea = document.querySelector(".list_area");
-	const addBtn = document.querySelector(".add_btn");
-	const cancleBtn = document.querySelector(".cancle_btn");
-
-	let toDoLists = [];
+let toDoLists = [];
 
 
-	const openModal = () => {
-		viewWrap.classList.add('display_none');
-		createWrap.classList.remove('display_none');
-	}
+const openModal = () => {
+	viewWrap.classList.add('display_none');
+	createWrap.classList.remove('display_none');
+}
 
-	const closeModal = () => {
-		viewWrap.classList.remove('display_none');
-		createWrap.classList.add('display_none');
+const closeModal = () => {
+	viewWrap.classList.remove('display_none');
+	createWrap.classList.add('display_none');
 
-		toDoTitle.value = "";
-		toDoContents.value = "";
-	}
+	toDoTitle.value = "";
+	toDoContents.value = "";
+}
 
-	const createTodoLists = (title) => {
-		const newId = toDoLists.length + 1;
-		const li = document.createElement('li');
-		li.id = newId;
+const createTodoLists = (title) => {
+	const newId = toDoLists.length + 1;
+	const createLi = document.createElement('li');
+	createLi.id = newId;
 
-		// <li id="todo_${newId}"></li>
-		const html = `
+	const html = `
 			<label for="checkBox">
 			<input type="checkbox" id="checkBox" />
 			<span>완료</span>
@@ -48,83 +45,72 @@
 			<button type="button">수정</button>
 			<button class="deleteBtn" type="button" onclick="deleteTodo(event)">삭제</button>
 		`;
-		li.innerHTML = html;
-		createWrap.appendChild(li);
+	createLi.innerHTML = html;
+	createWrap.appendChild(createLi);
 
+	const toDoItem = {
+		id: newId,
+		title: title,
+		createDate: Date.now(),
+		deadlineDate: Date.now(),
+		contents: toDoContents.value,
+		alramYN: false,
+		completeYN: false,
+		starYN: starYN.checked
+	};
 
-		// document.querySelector('.deleteBtn').addEventListener("click", deleteTodo);
+	toDoLists.push(toDoItem);
+	saveTodoLists();
+}
 
-		const toDoItem = {
-			id: newId,
-			title: title,
-			createDate: Date.now(),
-			deadlineDate: Date.now(),
-			contents: contents,
-			alramYN: false,
-			completeYN: false,
-			starYN: false
-		};
+const deleteTodo = (event) => {
+	console.log(event);
+	const selToDoList = event.target;
+	const selLiElement = selToDoList.parentNode;
+	selLiElement.parentNode.removeChild(selLiElement);
 
-		toDoLists.push(toDoItem);
-		saveTodoLists();
-	}
+	const cleanToDos = toDoLists.filter(function (toDo) {
+		return toDo.id !== parseInt(selLiElement.id);
+	});
+	toDoLists = cleanToDos;
+	console.log(cleanToDos);
+	saveTodoLists();
+}
 
-	const deleteTodo = (event) => {
-		console.log(event);
-		const btn = event.target;
-		const li = btn.parentNode;
-		console.log(li);
-		// toDoListArea.removeChild(li);
-		li.parentNode.removeChild(li);
+const saveTodoLists = () => {
+	localStorage.setItem(storageName, JSON.stringify(toDoLists));
+}
 
-		const cleanToDos = toDoLists.filter(function (toDo) {
-			return toDo.id !== parseInt(li.id);
+const handleSubmit = (e) => {
+	e.preventDefault();
+	if (!toDoTitle.value) { return; }
+
+	const title = toDoTitle.value;
+	createTodoLists(title);
+
+	toDoTitle.value = "";
+	toDoContents.value = "";
+}
+
+const loadToDoLists = () => {
+	const storage = localStorage.getItem(storageName);
+
+	if (!!storage) {
+		const parsedToDos = JSON.parse(storage);
+		parsedToDos.forEach((toDo) => {
+			createTodoLists(toDo.title);
 		});
-		toDoLists = cleanToDos;
-		console.log(cleanToDos);
-		saveTodoLists();
 	}
-
-	const saveTodoLists = () => {
-
-		localStorage.setItem(storageName, JSON.stringify(toDoLists));
-	}
-
-	const handleSubmit = (e) => {
-		console.log('??');
-		e.preventDefault();
-		if (!toDoTitle.value) {
-			console.log('no value');
-			return;
-		}
-		const title = toDoTitle.value;
-		const contents = toDoContents.value;
-		createTodoLists(title);
-		toDoTitle.value = "";
-		toDoContents.value = "";
-	}
-
-	const loadToDoLists = () => {
-		const storage = localStorage.getItem(storageName);
-
-		if (!!storage) {
-			const parsedToDos = JSON.parse(storage);
-			parsedToDos.forEach((toDo) => {
-				createTodoLists(toDo.title);
-			});
-		}
-	}
+}
 
 
-	// init ========
-	loadToDoLists();
+// init ========
+loadToDoLists();
 
-	toDoform.addEventListener("submit", handleSubmit);
-	toDoform.addEventListener("click", openModal);
-	addBtn.addEventListener("click", createTodoLists);
-	cancleBtn.addEventListener("click", closeModal);
+toDoform.addEventListener("submit", handleSubmit);
+toDoform.addEventListener("click", openModal);
+addBtn.addEventListener("click", createTodoLists);
+cancleBtn.addEventListener("click", closeModal);
 
 
-
-// })();
 
