959b582fcfe07145b0f5433d0c8405f19b5124ef
jexists <jexists486@gmail.com>
2021-02-26 00:04:52 +0900

    backup

--- excel_to_json/index.js
+++ excel_to_json/index.js
@@ -1,32 +1,47 @@
 console.log('스크립트 연결 완료!')
-
+let final = [];
 const input = document.getElementById('fileUpload');
 input.addEventListener('change', function () {
   readXlsxFile(input.files[0]).then(function (data) {
-    // console.log(data);
-    let i = 0;
-    data.map((data, index) => {
-      if (i == 0) {
-        let view = document.getElementById('jsonData');
-        generateArray(view, data);
+    for (let i = 1; i < data.length; i++) {
+
+      if (data[0] !== null) {
+        final.push({ 'group': data[0], 'team': [] });
+      } else {
+
+        console.log(data[i][5]);
       }
-      if (i > 0) {
-        let view = document.getElementById('jsonData');
-        generateObject(view, data)
-      }
-      i++;
-    })
+      console.log(data[i][0]);
+      console.log('index', i, data[i][5]);
+    }
   });
 });
 
-function generateArray(view, data) {
-  // console.log(view);
-  console.log('property', data);
-}
-const teams = []
-function generateObject(view, data) {
-  // console.log(jsonData);
-  teams.push(data[5]);
-  // console.log('team: [' + data[5] +']');
-  console.log(teams);
-}
+console.log(final);
+
+const view = document.getElementById('jsonData');
+view.innerHTML = final;
+
+
+// console.log('스크립트 연결 완료!')
+// let final= [];
+// const input = document.getElementById('fileUpload');
+// input.addEventListener('change', function () {
+//   readXlsxFile(input.files[0]).then(function (data) {
+//     // console.log(data);
+//     let i = 0;
+//     final = data.map((data) => {
+//       if (i > 0) {
+//         if (data[0] !== null) {
+//           final.push({'group': data[0], 'team':[]});
+//         }
+//         console.log(data[5]);
+//       }
+//       i++;
+//     });
+//   });
+// });
+// console.log(final);
+
+// const view = document.getElementById('jsonData');
+// view.innerHTML = final;
