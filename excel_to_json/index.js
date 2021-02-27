499b6581f874e6c3e20b23cb87d878e3ffc35b25
jexists <jexists486@gmail.com>
2021-02-28 00:15:34 +0900

    DONE!!!

--- excel_to_json/index.js
+++ excel_to_json/index.js
@@ -3,24 +3,30 @@ let final = [];
 const input = document.getElementById('fileUpload');
 input.addEventListener('change', function () {
   readXlsxFile(input.files[0]).then(function (data) {
+    let j = 0;
     for (let i = 1; i < data.length; i++) {
-
-      if (data[0] !== null) {
-        final.push({ 'group': data[0], 'team': [] });
+      if (data[i][0] !== null) {
+        final.push({ 'group': "'" + data[i][0] + "'", 'team': [data[i][5]] });
+        if(i !== 1) {
+          j++;
+        }
       } else {
-
-        console.log(data[i][5]);
+        final[j].team.push(data[i][5]);
       }
-      console.log(data[i][0]);
-      console.log('index', i, data[i][5]);
+      // console.log(data[i][0]);
+      // console.log('index', i, data[i][5]);
     }
+    console.log(final);
+
+    const JSONfinal = JSON.stringify(final).split('"').join('');
+    // console.log(JSONfinal.replace('"', ''));
+    console.log(JSONfinal);
+    const view = document.getElementById('jsonData');
+    view.innerHTML = JSONfinal;
   });
 });
+final = final;
 
-console.log(final);
-
-const view = document.getElementById('jsonData');
-view.innerHTML = final;
 
 
 // console.log('스크립트 연결 완료!')
