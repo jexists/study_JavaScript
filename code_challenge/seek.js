7dd78f7a410562d4520ab5ad677aabb8102fbd1a
jexists <jexists486@gmail.com>
2021-03-05 23:57:34 +0900

    seek using filter

--- code_challenge/seek.js
+++ code_challenge/seek.js
@@ -12,9 +12,7 @@ function destroyer(arr) {
   // const args = Array.from(arguments);
   // console.log(args); //[Array(6), 2, 3]
   // const targets = args.slice(1);
-  // console.log(targets); //[Array(6), 2, 3]
-  // const targets2 = args.slice(0, 1);
-  // console.log(targets2); //[Array(6)]
+  // console.log(targets); //[Array(6), 2, 3]gets2); //[Array(6)]
   var args = Array.from(arguments);
   args.splice(0, 1);
   var targets = args;
@@ -31,5 +29,19 @@ function destroyer(arr) {
   return result; //[2, 3]
 }
 
+// const targets2 = args.slice(0, 1);
+// console.log(tar
 destroyer([1, 2, 3, 1, 2, 3,], 2, 3); //[1, 1]
 console.log(destroyer([1, 2, 3, 1, 2, 3,], 2, 3));
+
+function destroyerFilter(arr) {
+  var args = Array.from(arguments);
+  args.splice(0, 1);
+  var targets = args;
+  var result = [];
+
+  return arr.filter((num) => {
+    return targets.indexOf(num) === -1;
+  })
+}
+console.log(destroyerFilter([1, 2, 3, 1, 2, 3,], 2, 3));
