930e0e04958540d19de27296ab10547dda9edf28
jexists <jexists486@gmail.com>
2021-04-07 22:15:52 +0900

    init

---
+++ wave/app.js
@@ -0,0 +1,33 @@
+
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+
+    this.ctx.scale(2, 2);
+  }
+
+  animate() {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    window.requestAnimationFrame(this.animate.bind(this))
+  }
+
+}
+window.onload = () => {
+  new App();
+}
