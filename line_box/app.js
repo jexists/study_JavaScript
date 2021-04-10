bd226a7bb78c705e6e284aa33fa5bb8f7c05f3e5
jexists <jexists486@gmail.com>
2021-04-10 23:46:10 +0900

    init

---
+++ line_box/app.js
@@ -0,0 +1,35 @@
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
+
+    thispixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this))
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+  }
+
+}
+window.onload = () => {
+  new App();
+}
