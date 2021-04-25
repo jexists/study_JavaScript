de346886d26b0c45e971ba31527b0f5a0cc417bc
jexists <jexists486@gmail.com>
2021-04-25 12:49:41 +0900

    timer function done

--- countdown-timer/index.js
+++ countdown-timer/index.js
@@ -1,6 +1,7 @@
 console.log('스크립트 연결 완료!')
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
@@ -8,7 +9,8 @@ function timer(seconds) {
 
   displayTimeLeft(seconds);
   // console.log({now, then});
-
+  displayEndTime(then);
+  
   countdown = setInterval(() => {
     // const secondsLeft = (then - Date.now()) / 1000; //소수점
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -33,6 +35,14 @@ function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
   const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`
+  document.title = display;
   timerDisplay.textContent = display;
 }
-// function 
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back at ${adjustedHour}:${minutes < 10 ? '0':''}${minutes}`;
+}
